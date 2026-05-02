bf4e401dff8bc5e472a0871b46877eff2d894e61
정찬원 <jcw0522@gachon.ac.kr>
2026-05-03 01:45:45 +0900

    feat: 가게 생성 스키마 적용

--- nunu/week6/src/modules/stores/repositories/store.repository.ts
+++ nunu/week6/src/modules/stores/repositories/store.repository.ts
@@ -1,46 +1,27 @@
-import { pool } from "../../../db.config.js";
-import { ResultSetHeader, RowDataPacket } from "mysql2";
+import { prisma } from "../../../db.config.js";
 import { CreateStoreRequest } from "../dtos/store.dto.js";
 
 // 가게 생성
 export const addStore = async (data: CreateStoreRequest) => {
-  const conn = await pool.getConnection();
+  const created = await prisma.store.create({
+    data: {
+      name: data.name,
+      storeType: data.storeType,
+      regionId: data.regionId,
+    },
+    select: {
+      id: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
 
-  try {
-    const [result] = await conn.query<ResultSetHeader>(
-      `INSERT INTO store (name, store_type, region_id)
-       VALUES (?, ?, ?)`,
-      [data.name, data.storeType, data.regionId],
-    );
-
-    const storeId = result.insertId;
-
-    // 생성된 데이터 조회
-    const [rows]: any = await conn.query(
-      `SELECT id, created_at, updated_at FROM store WHERE id = ?`,
-      [storeId],
-    );
-
-    return rows[0];
-  } catch (err) {
-    throw new Error(`오류가 발생했어요: ${err}`);
-  } finally {
-    conn.release();
-  }
+  return created;
 };
 
 // 지역 존재 확인
-export const getRegionById = async (
-  regionId: number,
-): Promise<RowDataPacket | null> => {
-  try {
-    const [rows] = await pool.query<RowDataPacket[]>(
-      `SELECT * FROM region WHERE id = ?`,
-      [regionId],
-    );
-
-    return rows[0] || null;
-  } catch (err) {
-    throw new Error(`오류가 발생했어요: ${err}`);
-  }
+export const getRegionById = async (regionId: number) => {
+  return await prisma.region.findFirst({
+    where: { id: regionId },
+  });
 };
