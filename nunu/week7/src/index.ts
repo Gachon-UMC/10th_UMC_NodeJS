077b0bca1d35779520a1416cceb8af69d77a6c15
정찬원 <jcw0522@gachon.ac.kr>
2026-05-08 11:39:09 +0900

    쿠키 파서 적용

--- nunu/week7/src/index.ts
+++ nunu/week7/src/index.ts
@@ -4,12 +4,15 @@ import cors from "cors";
 import errorHandler from "./middlewares/errorHandler.js";
 import { RegisterRoutes } from "./generated/routes.js";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 // 1. 환경 변수 설정
 dotenv.config();
 
 const app: Express = express();
 app.use(morgan("dev")); // 로그 포맷: dev
+app.use(cookieParser());
+
 const port = process.env.PORT || 3000;
 
 // 2. 미들웨어 설정
@@ -25,6 +28,26 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/test", (req, res) => {
   res.send("Hello!");
+});
+
+// 쿠키 만드는 라우터
+app.get("/setcookie", (req, res) => {
+  // 'myCookie'라는 이름으로 'hello' 값을 가진 쿠키를 생성
+  res.cookie("myCookie", "hello", { maxAge: 60000 }); // 60초간 유효
+  res.send("쿠키가 생성되었습니다!");
+});
+
+// 쿠키 읽는 라우터
+app.get("/getcookie", (req, res) => {
+  // cookie-parser 덕분에 req.cookies 객체에서 바로 꺼내 쓸 수 있음
+  const myCookie = req.cookies.myCookie;
+
+  if (myCookie) {
+    console.log(req.cookies); // { myCookie: 'hello' }
+    res.send(`당신의 쿠키: ${myCookie}`);
+  } else {
+    res.send("쿠키가 없습니다.");
+  }
 });
 
 // 4. API 라우트
